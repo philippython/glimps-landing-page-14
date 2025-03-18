8a8a2304c68b15d6a78209fed460138a208d347a
Alex Tran <tranduclong092@gmail.com>
2025-03-18 10:28:20 +0200

    Refactor: use uuid instead of testing data in fetch photos

--- src/service/fetchPhotosFromApi.ts
+++ src/service/fetchPhotosFromApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchPhotosFromApi = async (uuid: string) => {
   try {
-    const res = await axios.get(`${import.meta.env.VITE_API_URL}/link/${import.meta.env.VITE_TESTING_LINK_ID}`);
+    const res = await axios.get(`${import.meta.env.VITE_API_URL}/link/${uuid}`);
     return res.data;
   } catch (error) {
     console.error("Error fetching photos from API", error);
