fb6f1e07aac03d1e40d36fbf2dbf3964aa13a6f2
Alex Tran <tranduclong092@gmail.com>
2025-03-19 11:13:08 +0200

    Feat: add a mock login and logout function

--- src/service/AuthProvider.tsx
+++ src/service/AuthProvider.tsx
@@ -15,6 +15,23 @@ export const AuthProvider = ({ children }) => {
     setLoading(false);
   }, []);
 
+  const login = async (email: string, password: string) => {
+    try {
+      console.log("Logging in with creadential:", email, password);
+      localStorage.setItem("user", JSON.stringify("user"));
+      localStorage.setItem("token", "token");
+    } catch (error) {
+      console.error("Login error:", error);
+      throw error;
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
   return (
     <AuthContext.Provider value={{ user, loading }}>
       {children}
