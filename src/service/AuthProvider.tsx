a0fc059db72705650ecd12e97e146592435f2217
Alex Tran <tranduclong092@gmail.com>
2025-03-19 11:07:10 +0200

    Feat: add states and on load check to the auth provider context

--- src/service/AuthProvider.tsx
+++ src/service/AuthProvider.tsx
@@ -1,10 +1,22 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // check login status on page load
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+    setLoading(false);
+  }, []);
+
   return (
-    <AuthContext.Provider value={null}>
+    <AuthContext.Provider value={{ user, loading }}>
       {children}
     </AuthContext.Provider>
   )
