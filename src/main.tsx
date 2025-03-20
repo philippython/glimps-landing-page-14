197ee89277c56d914c37583579c2f6a9e735e87b
Alex Tran <tranduclong092@gmail.com>
2025-03-20 11:30:02 +0200

    Refactor: move AuthProvider component to a seperate folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { AuthProvider } from './service/AuthProvider.tsx';
+import { AuthProvider } from './auth/AuthProvider';
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
