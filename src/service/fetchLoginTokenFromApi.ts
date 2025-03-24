7e6b0165aead6c0e4b480db5bcd1205d3d8d3132
Alex Tran <tranduclong092@gmail.com>
2025-03-24 09:30:55 +0200

    Feat: create axios call to backend to get login token

--- src/service/fetchLoginTokenFromApi.ts
+++ src/service/fetchLoginTokenFromApi.ts
@@ -2,7 +2,16 @@ import axios from "axios";
 
 export const fetchLoginTokenFromApi = async (username, password) => {
   try {
-    console.log("fetchLoginTokenFromApi", username, password);
+    const res = await axios.post(
+      `${import.meta.env.VITE_API_URL}/token`,
+      `grant_type=password&username=${username}&password=${password}&scope=&client_id=&client_secret=`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Accept": "application/json",
+        },
+      }
+    )
   } catch (error) {
     console.error("Error fetching login token from API", error);
     throw error;
