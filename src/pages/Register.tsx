0b3b511f03bf8990e1ae6a4165a0d9dbf9c2e582
Alex Tran <tranduclong092@gmail.com>
2025-04-01 11:44:21 +0300

    Feat: add postNewUserToApi function to Register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -24,6 +24,7 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
+import { postNewUserToApi } from "@/service/postNewUserToApi";
 
 type Steps = "register" | "venue";
 
@@ -67,11 +68,12 @@ const Register = () => {
     setLoading(true);
     try {
       // Simulate API call
-      await new Promise(resolve => setTimeout(resolve, 1500));
-      console.log(values);
+      const res = await postNewUserToApi(values.username, values.email, values.password);
+      if (res.username) {
+        toast.success("Account created successfully!");
+        setStep("venue");
+      }
 
-      setStep("venue");
-      toast.success("Account created successfully!");
     } catch (error) {
       toast.error("Registration failed. Please try again.");
     } finally {
--- src/service/postNewUserToApi.ts
+++ src/service/postNewUserToApi.ts
@@ -1,25 +1,37 @@
 import axios from "axios";
-import { LoginTokenResponse } from "./fetchLoginTokenFromApi";
+import { UserData } from "./fetchLoginTokenFromApi";
 
 export const postNewUserToApi = async (username: string, email: string, password: string) => {
   try {
-    const res = await axios.post<LoginTokenResponse>(
-      `${import.meta.env.VITE_API_URL}/auth-user/create`,
-      {
-        email,
-        password,
-        username,
-        role: "renter",
-        invite_code: ""
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-      }
-    )
-    return res.data;
+    // const res = await axios.post<UserData>(
+    //   `${import.meta.env.VITE_API_URL}/auth-user/create`,
+    //   {
+    //     email,
+    //     password,
+    //     username,
+    //     role: "renter",
+    //     invite_code: ""
+    //   },
+    //   {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "Accept": "application/json",
+    //     },
+    //   }
+    // )
+    // return res.data;
+
+    // test data
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    return {
+      "id": "b959cc12-81a3-4065-aa17-502f84d0433b",
+      "email": "test2@example.com",
+      "username": "testuser2",
+      "role": "renter",
+      "activated": true,
+      "venue": null,
+      "created_at": "2025-04-01T08:04:04.899734"
+    };
   } catch (error) {
     console.error("Error fetching login token from API", error);
     throw error;
