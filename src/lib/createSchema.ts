5e783b5683e84315a3d0ad4d238b33a69de352c5
Alex Tran <tranduclong092@gmail.com>
2025-04-03 14:48:56 +0300

    Feat: add localization for messages in createSchema

--- src/lib/createSchema.ts
+++ src/lib/createSchema.ts
@@ -4,18 +4,34 @@ import { z } from "zod";
 
 export const createSchema = (intl: IntlShape) => {
   const createFormSchema = z.object({
-    name: z.string().min(2, { message: "Venue name must be at least 2 characters." }),
-    contact_num: z.string().min(5, { message: "Contact number must be at least 5 characters." }),
-    venue_logo: z.instanceof(File, { message: "Logo is required" })
+    name: z.string().min(2, {
+      message: intl.formatMessage({
+        id: "venueDashboard.venueSettings.messages.venueNameTooShort"
+      })
+    }),
+    contact_num: z.string().min(5, {
+      message: intl.formatMessage({
+        id: "venueDashboard.venueSettings.messages.contactNumberTooShort"
+      })
+    }),
+    venue_logo: z.instanceof(File, {
+      message: intl.formatMessage({
+        id: "venueDashboard.venueSettings.messages.missingLogo"
+      })
+    })
       .refine(
         file => file.size <= import.meta.env.VITE_LOGO_MAX_FILE_SIZE * 1024 * 1024,
-        "Max file size is 5MB."
+        intl.formatMessage({
+          id: "venueDashboard.venueSettings.messages.exceededLogoSize"
+        })
       )
       .refine(
         file => import.meta.env.VITE_ACCEPTED_LOGO_FILE_TYPES
           .split(",")
           .includes(file.type),
-        "Only .jpg, .jpeg, and .png formats are supported."
+        intl.formatMessage({
+          id: "venueDashboard.venueSettings.messages.invalidLogoType"
+        })
       ),
     logo_position: z.nativeEnum(LogoPosition),
     logo_ratio: z.array(z.number().min(1).max(100)),
@@ -23,18 +39,34 @@ export const createSchema = (intl: IntlShape) => {
   });
 
   const editFormSchema = z.object({
-    name: z.string().min(2, { message: "Venue name must be at least 2 characters." }),
-    contact_num: z.string().min(5, { message: "Contact number must be at least 5 characters." }),
-    venue_logo: z.instanceof(File, { message: "Logo must be a valid file" })
+    name: z.string().min(2, {
+      message: intl.formatMessage({
+        id: "venueDashboard.venueSettings.messages.venueNameTooShort"
+      })
+    }),
+    contact_num: z.string().min(5, {
+      message: intl.formatMessage({
+        id: "venueDashboard.venueSettings.messages.contactNumberTooShort"
+      })
+    }),
+    venue_logo: z.instanceof(File, {
+      message: intl.formatMessage({
+        id: "venueDashboard.venueSettings.messages.missingLogo"
+      })
+    })
       .refine(
-        file => file.size <= import.meta.env.VITE_LOGO_MAX_FILE_SIZE * 1024 * 1024
-        , "Max file size is 5MB."
+        file => file.size <= import.meta.env.VITE_LOGO_MAX_FILE_SIZE * 1024 * 1024,
+        intl.formatMessage({
+          id: "venueDashboard.venueSettings.messages.exceededLogoSize"
+        })
       )
       .refine(
         file => import.meta.env.VITE_ACCEPTED_LOGO_FILE_TYPES
           .split(",")
           .includes(file.type),
-        "Only .jpg, .jpeg, and .png formats are supported."
+        intl.formatMessage({
+          id: "venueDashboard.venueSettings.messages.invalidLogoType"
+        })
       )
       .optional(),
     logo_position: z.nativeEnum(LogoPosition),
