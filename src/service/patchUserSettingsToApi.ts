c9ee741bc02b785cef535e694c184e8f515df5f1
Alex Tran <tranduclong092@gmail.com>
2025-04-04 10:01:44 +0300

    Init: create service function to patch user data

---
+++ src/service/patchUserSettingsToApi.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { UserData, VenueData } from "./fetchLoginTokenFromApi";
+import { ProfileFormValues } from "@/components/AccountSettings";
+
+export type PatchUserSettingsToApiResponse = {
+  "id": string,
+  "venue": VenueData,
+}
+
+export const patchUserSettingsToApi = async (values: ProfileFormValues, token: string, user: UserData) => {
+  try {
+    const res = await axios.patch<PatchUserSettingsToApiResponse>(
+      `${import.meta.env.VITE_API_URL}/auth-user/${user.id}`,
+      {
+        "email": values.email,
+        "password": "",
+        "username": values.username,
+        "role": user.role,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+      }
+    );
+
+    return res.data;
+  } catch (error) {
+    console.error("Error updating user data to API", error);
+    throw error;
+  }
+}
